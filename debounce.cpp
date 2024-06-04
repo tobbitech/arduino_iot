940a70d5842b4c2adb611f1bd3fe6f9e8e0f9424
tobbitech <torbjorn.cederlov@gmail.com>
2024-06-04 12:58:59 +0200

    added sticky button

--- debounce.cpp
+++ debounce.cpp
@@ -10,7 +10,21 @@ DebounceButton::DebounceButton(int pin, u_int32_t debounce_delay) {
     _last_debounce_time = 0;
     _is_pressed = false;
     _is_released = false;
+    _sticky_timer.set(0, "seconds");
+    _is_sticky_held = false;
 
+}
+
+void DebounceButton::set_sticky_button_timer(Timer sticky_timer) {
+    _sticky_timer = sticky_timer;
+}
+
+uint32_t DebounceButton::get_remaining_sticky_hold_time_ms() {
+    return(_sticky_timer.remaining() );
+}
+
+bool DebounceButton::is_sticky_held() {
+    return (_is_sticky_held);
 }
 
 bool DebounceButton::is_pressed() {
@@ -32,6 +46,10 @@ u_int32_t DebounceButton::get_hold_time_ms() {
 void DebounceButton::tick() {
     switch_value = digitalRead(_pin);
     _last_state = _state;
+
+    if (_sticky_timer.is_done() ) {
+        _is_sticky_held = false;
+    }
 
 
     switch(_state) {
@@ -65,6 +83,9 @@ void DebounceButton::tick() {
             _is_pressed = true;
             _hold_time_ms = millis();
             // Serial.println("Button pressed");
+            // start sticky button timer
+            _sticky_timer.reset();
+            _is_sticky_held = true;
             break;
 
         case DebounceButton::HELD:
